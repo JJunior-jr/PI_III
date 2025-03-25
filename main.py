ca4be08fb97c40bd9f7edc48c3eb5f399b8b1f7b
Junior <jacsonsajr.study@gmail.com>
2025-03-25 20:14:21 -0300

    atualizacao do main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import app
 from fastapi import FastAPI, Request, Form, HTTPException, Depends, Cookie, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -6,23 +7,34 @@ from fastapi.staticfiles import StaticFiles
 from fastapi import Response
 import json
 from typing import Optional
+
 from starlette.responses import JSONResponse
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 def conectar_banco():
     return mysql.connector.connect(
-        host="127.0.0.1",
+        host="db",
         user="root",
-        password="teste",
-        database="bd_pi3"
+        password="Allstar3",
+        database="estoque",
+        port=3306
+
     )
 
-def criar_tabela_usuario():
+def criar_tabelas():
     conexao = conectar_banco()
     cursor = conexao.cursor()
+
+    # Cria o banco de dados se não existir
+    cursor.execute("CREATE DATABASE IF NOT EXISTS estoque")
+    cursor.execute("USE estoque")
+
+    # Cria a tabela usuario
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS usuario (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,15 +42,8 @@ def criar_tabela_usuario():
             senha VARCHAR(255) NOT NULL
         )
     """)
-    conexao.commit()
-    cursor.close()
-    conexao.close()
 
-criar_tabela_usuario() # Cria a tabela ao iniciar a aplicação
-
-def criar_tabela_produtos():
-    conexao = conectar_banco()
-    cursor = conexao.cursor()
+    # Cria a tabela produto
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS produto (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -48,15 +53,8 @@ def criar_tabela_produtos():
             vonixx_sintra INT(10)
         )
     """)
-    conexao.commit()
-    cursor.close()
-    conexao.close()
 
-criar_tabela_produtos()
-
-def criar_tabela_agendamento():
-    conexao = conectar_banco()
-    cursor = conexao.cursor()
+    # Cria a tabela agendamento
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS agendamento (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -71,11 +69,22 @@ def criar_tabela_agendamento():
             status VARCHAR(20) DEFAULT 'pendente'
         )
     """)
+
+    # Verifica se a tabela usuario está vazia e insere dados se necessário
+    cursor.execute("SELECT COUNT(*) FROM usuario")
+    if cursor.fetchone()[0] == 0:
+        cursor.execute("INSERT INTO usuario (nome, senha) VALUES ('Admin', 123)")
+
+    # Verifica se a tabela produto está vazia e insere dados se necessário
+    cursor.execute("SELECT COUNT(*) FROM produto")
+    if cursor.fetchone()[0] == 0:
+        cursor.execute("INSERT INTO produto (vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra) VALUES (1500, 1500, 1500, 1500)")
+
     conexao.commit()
     cursor.close()
     conexao.close()
 
-criar_tabela_agendamento()
+criar_tabelas() # Cria as tabelas e insere dados ao iniciar a aplicação
 
 def obter_usuario(usuario_id: int):
     conexao = conectar_banco()
@@ -125,11 +134,13 @@ async def pagina_principal(request: Request, response: Response):
     usuario_id = request.cookies.get("usuario_id")
     if not usuario_id:
         return RedirectResponse(url="/login", status_code=303)
-    
+
     usuario = obter_usuario(int(usuario_id))  # Obtenha o usuário do banco de dados
+
     if not usuario:
         response.delete_cookie("usuario_id")  # Remova o cookie se o usuário não for encontrado
         return RedirectResponse(url="/login", status_code=303)
+
     return templates.TemplateResponse("pagina_principal.html", {"request": request, "usuario": usuario})
 
 # Login e usuários
@@ -147,12 +158,14 @@ async def login(nome: str = Form(...), senha: str = Form(...), response: Respons
     usuario = cursor.fetchone()
     cursor.close()
     conexao.close()
+
     if usuario:
         response = RedirectResponse(url="/", status_code=303)
         response.set_cookie(key="usuario_id", value=str(usuario['id']))
         return response
     else:
         return RedirectResponse(url="/login?error_message=Credenciais inválidas", status_code=303)
+
 @app.get("/logout")
 async def logout(response: Response):
     response.delete_cookie(key="usuario_id")  # Remove o cookie ao fazer logout
@@ -214,15 +227,19 @@ async def excluir_usuario(usuario_id: int):
     return RedirectResponse(url="/dashboard", status_code=303)
 
 # Agendamentos
+
 @app.get("/agendamentos", response_class=HTMLResponse)
 async def listar_agendamentos(request: Request):
     conexao = conectar_banco()
     cursor = conexao.cursor(dictionary=True)
+
     # Busca todos os agendamentos
     cursor.execute("SELECT * FROM agendamento")
     agendamentos = cursor.fetchall()
+
     cursor.close()
     conexao.close()
+
     return templates.TemplateResponse("agendamentos_lista.html", {"request": request, "agendamentos": agendamentos})
 
 @app.get("/agendamentos/novo", response_class=HTMLResponse)
@@ -242,36 +259,38 @@ async def criar_agendamento(
 ):
     conexao = conectar_banco()
     cursor = conexao.cursor()
+
     # Obter estoque atual dos produtos
-    cursor.execute("SELECT * FROM produto WHERE id = 1")  # Assumindo que há apenas 1 registro na tabela produto
+    cursor.execute("SELECT * FROM produto WHERE id = 1") # Assumindo que há apenas 1 registro na tabela produto
     produto = cursor.fetchone()
+
     if produto:
         # Verificar estoque suficiente
         if (produto[1] >= vonixx_extractus and
             produto[2] >= vonixx_bactran and
             produto[3] >= vonixx_sanitizante and
             produto[4] >= vonixx_sintra):
+
             # Subtrair produtos do estoque
             novo_extractus = produto[1] - vonixx_extractus
             novo_bactran = produto[2] - vonixx_bactran
             novo_sanitizante = produto[3] - vonixx_sanitizante
             novo_sintra = produto[4] - vonixx_sintra
+
             cursor.execute("""
                 UPDATE produto
-                SET vonixx_extractus = %s, vonixx_bactran = %s, 
-                vonixx_sanitizante = %s, vonixx_sintra = %s
+                SET vonixx_extractus = %s, vonixx_bactran = %s, vonixx_sanitizante = %s, vonixx_sintra = %s
                 WHERE id = 1
             """, (novo_extractus, novo_bactran, novo_sanitizante, novo_sintra))
+
             # Inserir agendamento
             sql = """
-                INSERT INTO agendamento (cliente, endereco, telefone, 
-                data_agendamento, vonixx_extractus, vonixx_bactran, vonixx_sanitizante, 
-                vonixx_sintra)
+                INSERT INTO agendamento (cliente, endereco, telefone, data_agendamento, vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """
-            val = (cliente, endereco, telefone, data_agendamento, 
-                   vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra)
+            val = (cliente, endereco, telefone, data_agendamento, vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra)
             cursor.execute(sql, val)
+
             conexao.commit()
             cursor.close()
             conexao.close()
@@ -283,7 +302,12 @@ async def criar_agendamento(
     else:
         cursor.close()
         conexao.close()
-        raise HTTPException(status_code=404, detail="Produto não encontrado.") 
+        raise HTTPException(status_code=404, detail="Produto não encontrado.")
+
+@app.get("/agendamentos/{agendamento_id}/editar", response_class=HTMLResponse)
+async def agendamento_editar_form(request: Request, agendamento: dict = Depends(obter_agendamento)):
+    return templates.TemplateResponse("agendamento_editar.html", {"request": request, "agendamento": agendamento})
+
 @app.post("/agendamentos/{agendamento_id}/editar")
 async def atualizar_agendamento(
     agendamento_id: int,
@@ -300,13 +324,10 @@ async def atualizar_agendamento(
     cursor = conexao.cursor()
     sql = """
         UPDATE agendamento
-        SET cliente = %s, endereco = %s, telefone = %s, data_agendamento = %s, 
-        vonixx_extractus = %s, vonixx_bactran = %s, vonixx_sanitizante = %s, 
-        vonixx_sintra = %s
+        SET cliente = %s, endereco = %s, telefone = %s, data_agendamento = %s, vonixx_extractus = %s, vonixx_bactran = %s, vonixx_sanitizante = %s, vonixx_sintra = %s
         WHERE id = %s
     """
-    val = (cliente, endereco, telefone, data_agendamento, vonixx_extractus, 
-           vonixx_bactran, vonixx_sanitizante, vonixx_sintra, agendamento_id)
+    val = (cliente, endereco, telefone, data_agendamento, vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra, agendamento_id)
     cursor.execute(sql, val)
     conexao.commit()
     cursor.close()
@@ -317,22 +338,27 @@ async def atualizar_agendamento(
 async def finalizar_agendamento(id: int):
     conexao = conectar_banco()
     cursor = conexao.cursor()
+
     # Verifica se o agendamento existe
     cursor.execute("SELECT * FROM agendamento WHERE id = %s", (id,))
     agendamento = cursor.fetchone()
+
     if agendamento:
         # Atualiza o status do agendamento para "concluído"
         sql = "UPDATE agendamento SET status = 'concluído' WHERE id = %s"
         val = (id,)
         cursor.execute(sql, val)
+
         conexao.commit()
         cursor.close()
         conexao.close()
+
         return RedirectResponse(url="/agendamentos", status_code=303)
     else:
         cursor.close()
         conexao.close()
         raise HTTPException(status_code=404, detail="Agendamento não encontrado")
+
 
 # Produtos
 @app.get("/produtos", response_class=HTMLResponse)
@@ -344,7 +370,7 @@ async def listar_produtos(request: Request):
     produtos = cursor.fetchall()
     cursor.close()
     conexao.close()
-    return templates.TemplateResponse("produtos_lista.html", {"request": request, "produtos": produtos})
+    return templates.TemplateResponse("produtos_lista.html", {"request": request,  "produtos": produtos})
 
 @app.get("/produtos/novo", response_class=HTMLResponse)
 async def produto_form(request: Request):
@@ -360,8 +386,7 @@ async def criar_produto(
     conexao = conectar_banco()
     cursor = conexao.cursor()
     sql = """
-        INSERT INTO produto (vonixx_extractus, vonixx_bactran, 
-        vonixx_sanitizante, vonixx_sintra)
+        INSERT INTO produto (vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra)
         VALUES (%s, %s, %s, %s)
     """
     val = (vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra)
@@ -387,8 +412,7 @@ async def atualizar_produto(
     cursor = conexao.cursor()
     sql = """
         UPDATE produto
-        SET vonixx_extractus = %s, vonixx_bactran = %s, vonixx_sanitizante = %s, 
-        vonixx_sintra = %s
+        SET vonixx_extractus = %s, vonixx_bactran = %s, vonixx_sanitizante = %s, vonixx_sintra = %s
         WHERE id = %s
     """
     val = (vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra, produto_id)
@@ -410,25 +434,31 @@ async def finalizar_produto(produto_id: int):
     conexao.close()
     return RedirectResponse(url="/produtos", status_code=303)
 
+
 @app.get("/relatorios", response_class=HTMLResponse)
 async def pagina_relatorios(request: Request):
     conexao = conectar_banco()
     cursor = conexao.cursor(dictionary=True)
+
     # Dados para gráfico de agendamentos
     cursor.execute("SELECT DATE_FORMAT(data_agendamento, '%Y-%m-%d') AS mes, COUNT(*) AS quantidade FROM agendamento GROUP BY mes")
     agendamentos = cursor.fetchall()
     agendamentos_meses = [a['mes'] for a in agendamentos]
     agendamentos_quantidades = [int(a['quantidade']) for a in agendamentos]  # Convertendo para int
+
     # Dados para gráfico de produtos utilizados
     cursor.execute("SELECT SUM(vonixx_extractus) AS extractus, SUM(vonixx_bactran) AS bactran, SUM(vonixx_sanitizante) AS sanitizante, SUM(vonixx_sintra) AS sintra FROM agendamento")
     produtos_utilizados = cursor.fetchone() or {}
     produtos_utilizados = {k: int(v) if v is not None else 0 for k, v in produtos_utilizados.items()}  # Convertendo para int
+
     # Dados para gráfico de estoque de produtos
     cursor.execute("SELECT vonixx_extractus, vonixx_bactran, vonixx_sanitizante, vonixx_sintra FROM produto")
     estoque_produtos = cursor.fetchone() or {}
     estoque_produtos = {k: int(v) if v is not None else 0 for k, v in estoque_produtos.items()}  # Convertendo para int
+
     cursor.close()
     conexao.close()
+
     return templates.TemplateResponse("relatorios.html", {
         "request": request,
         "agendamentos_meses": agendamentos_meses,
@@ -446,8 +476,11 @@ async def listar_agendamentos_api(
 ):
     conexao = conectar_banco()
     cursor = conexao.cursor(dictionary=True)
+
     sql = "SELECT * FROM agendamento WHERE 1=1"  # Inicia com uma condição sempre verdadeira
+
     params = []
+
     if status:
         sql += " AND status = %s"
         params.append(status)
@@ -457,10 +490,13 @@ async def listar_agendamentos_api(
     if data_fim:
         sql += " AND data_agendamento <= %s"
         params.append(data_fim)
+
     cursor.execute(sql, params)
     agendamentos = cursor.fetchall()
+
     cursor.close()
     conexao.close()
+
     return JSONResponse(content=agendamentos)
 
 @app.get("/api/agendamentos/{agendamento_id}")
